80ab49d99a8f1952da853ac1f2946357dfa24fee
ehdi <ehdi@DESKTOP-1333PRG>
2024-02-04 23:40:36 +0800

    添加项目文件。

---
+++ FileOperate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperateTool
+{
+    interface FileTool
+    {
+        
+    }
+    public class FileOperate
+    {
+        public static void 
+    }
+}
